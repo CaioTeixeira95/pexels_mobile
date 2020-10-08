b844d2884ea3a2d2dfa6d36d69145fbc9177329c
Caio Teixeira <caio@pop-os.localdomain>
2020-10-07 21:06:24 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:pexels/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+// API KEY: 563492ad6f917000010000015cb98c77d469483589975337f4bbebfa
